066c4263e5d983d9b0d95d733d39103b7e4167d8
viinilv <83708990+luisviniciuslv@users.noreply.github.com>
2025-02-13 21:03:10 +0000

    starting cli

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,26 @@
-use excelize::{Cell, Spreadsheet};
-use polars::prelude::*;
-use regex::Regex;
-
-const NAME_SHEET_OMIE: &str = "Contratos por Cliente";
-
-#[derive(Debug)]
-pub struct Column {
-    index: u32,
-    itens: Vec<String>,
-}
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let column_cnum_cobranca = get_cnum_cobrancas();
-    let column_cnum_omie = get_cnums_omie();
-
-    let mut missing_items = Vec::new();
-    for item in column_cnum_omie.itens {
-        if !column_cnum_cobranca.itens.contains(&item) {
-            missing_items.push(item);
-        }
-    }
-
-    if missing_items.is_empty() {
-        println!("Nenhum item faltante");
-    } else {
-        println!("Itens faltantes: ");
-        for item in missing_items {
-            println!("{}", item);
-    }
+    let message = String::from("Escolha uma opção:
+    [1] Comparar colunas de duas planilhas\n");
+    let res = input(message);
+    println!("{}", res)
 }
 
-fn get_cnums_omie() -> Column {
-    let mut column_cnum_omie = Column {
-        index: 5,
-        itens: Vec::new(),
-    };
-    let column_qtd_omie = Column {
-        index: 8,
-        itens: Vec::new(),
-    };
+fn input(question: String) -> String {
+    let mut s = String::new();
+    print!("{}", question.to_string());
 
-    let ws = Spreadsheet::open_file(String::from("planilhas/pivot.xlsx")).unwrap();
-    let mut finish = false;
-    let mut counter = 3;
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Algo deu errado");
 
-    while !finish {
-        let cell = ws
-            .get_cell_value(NAME_SHEET_OMIE, counter, column_qtd_omie.index)
-            .unwrap();
-
-        if cell.is_empty() {
-            finish = true
-        }
-
-        let cnumctr = ws
-            .get_cell_value(NAME_SHEET_OMIE, counter, column_cnum_omie.index)
-            .unwrap();
-
-        if !cnumctr.is_empty() {
-            column_cnum_omie.itens.push(cnumctr);
-        }
-        counter += 1;
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
     }
 
-    column_cnum_omie
-}
-
-fn get_cnum_cobrancas() -> Column {
-    let mut column_cnum_asaas = Column {
-        index: 3,
-        itens: Vec::new(),
-    };
-
-    let df_cobrancas = CsvReadOptions::default()
-        .try_into_reader_with_file_path(Some("planilhas/cobrancas.csv".into()))
-        .unwrap()
-        .finish()
-        .unwrap();
-
-    let mut finish = false;
-    let mut count = 2;
-    let re = Regex::new(r"(\d{4}/\d{5})").unwrap();
-
-    while !finish {
-        match df_cobrancas.column("Nome").unwrap().get(count) {
-            Ok(item) => {
-                for cap in re.captures_iter(&item.to_string()) {
-                    column_cnum_asaas.itens.push(cap[0].to_string());
-                }
-                count += 1;
-            }
-            Err(_e) => finish = true,
-        }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
     }
 
-    column_cnum_asaas
-    }
+    s
 }
